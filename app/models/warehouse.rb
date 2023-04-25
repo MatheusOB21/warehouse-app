9507601c956cfdb14040b0f35cfd4d3defc8aa9d
MatheusOB21 <matheus53barros@hotmail.com>
2023-04-25 18:44:33 -0400

    Garantido que o CEP seja válido no formato: 00000-000.|Testes ok

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,6 @@
 class Warehouse < ApplicationRecord
     validates :name,:description, :code, :adress, :city, :cep, :area, presence: true
     validates :code, uniqueness: true
+    validates :cep, length: { is: 9 }
+    validates :cep , format: { with: /\d{5}-\d{3}/ }
 end
