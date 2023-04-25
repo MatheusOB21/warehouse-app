33f03f966c0ea2af000e4ed785f2821c5b01c40e
MatheusOB21 <matheus53barros@hotmail.com>
2023-04-25 19:00:19 -0400

    Feito o teste para validação de CNPJ ser 14 dígitos

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -29,5 +29,15 @@ RSpec.describe Supplier, type: :model do
           #Assert
             expect(result).to eq(false)
         end
+
+        it "false when registration_number is different of 13 digits" do
+          #Arrange
+            supplier1 = Supplier.create!(corporate_name: "Cargo LTA", brand_name: "Cargueiro", registration_number:"90256745611345", full_address: "Rua Law, 256", city: "Fortaleza", state: "CE", email: "contato@misterysa.com.br")
+            supplier2 = Supplier.new(corporate_name: "Capanga SA", brand_name: "Monster", registration_number:"9025674561", full_address: "Rua Luiz XV, 280", city: "São Luis", state: "MA", email: "contato@capanga.com.br")
+          #Act
+            result = supplier2.valid?
+          #Assert
+            expect(result).to eq(false)
+        end
     end
 end
